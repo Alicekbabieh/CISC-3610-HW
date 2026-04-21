dc6641e9580c82b0235942adbc7b71e538cab314
Alicekbabieh <alicekbabieh@gmail.com>
2026-04-21 11:25:35 -0400

    Create CodingWithAI.js

---
+++ CodingWithAI.js
@@ -0,0 +1,15 @@
+/*
+Write a JavaScript function that changes the background to a random color when 'colorBtn' is clicked. Explain each line.
+*/
+document.getElementById("colorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
